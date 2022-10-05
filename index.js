1a008b8230563efb884ded25fb9aa14178921a44
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-10-05 11:15:54 +0630

    v-for

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const app = Vue.createApp({
     data(){
         return {
-            title : "vue 3 start",
-            author : "Kaung Min Khant",
-            age : 20,
+            books:[
+                {
+                    title : "vue 1 start",
+                    author : "Kaung Min Khant",
+                    age : 20,
+                   
+                },
+                {
+                    title : "vue 2 start",
+                    author : "Kaung  Khant",
+                    age : 22,
+                    
+                },
+                {
+                    title : "vue 3 start",
+                    author : "Min Khant",
+                    age : 24,
+                   
+                },
+            ],
             ShowBook:true
+          
         }
     },
     methods:{
